86fad43fdd8df1fc5a96e2b8469b8240d033dfaa
higunito <reisen0615akira@gmail.com>
2025-03-01 11:50:09 +0900

    マウスの動きに応じて光の位置を変更する機能を追加し、スタイルを調整して背景をグラデーションに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
 
 function App() {
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const light = document.getElementById('light');
+      if (light) {
+        const lightWidth = light.offsetWidth;
+        const lightHeight = light.offsetHeight;
+        light.style.left = `${event.clientX - lightWidth / 2}px`;
+        light.style.top = `${event.clientY - lightHeight / 2}px`;
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <h1>React App</h1>
-        </header>
-      </div>
+      <div id="light"></div>
+      <div className="overlay"></div>
+      <h1 className="text">R3F Sample</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
